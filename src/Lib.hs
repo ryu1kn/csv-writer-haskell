49b6b88c333ed3c1776aadc13a396bd78f97cf35
Ryuichi Inagaki <ryuichi.inagaki.24@googlemail.com>
2016-11-09 00:30:25 +1100

    Escape double quotes

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,8 +9,15 @@ stringifyField = quoteIfNecessary . show . wrap
 
 quoteIfNecessary :: String -> String
 quoteIfNecessary xs
-    | ',' `elem` xs  = quote xs
+    |  ',' `elem` xs = quote xs
     | '\n' `elem` xs = quote xs
+    |  '"' `elem` xs = quote xs
     | otherwise      = xs
-    where
-        quote xs = concat ["\"", xs, "\""]
+
+quote :: String -> String
+quote xs = concat ["\"", escapeQuotes xs, "\""]
+
+escapeQuotes :: String -> String
+escapeQuotes []       = []
+escapeQuotes ('"':xs) = '"' : '"' : escapeQuotes xs
+escapeQuotes (  x:xs) = x : escapeQuotes xs
--- test/Spec.hs
+++ test/Spec.hs
@@ -13,6 +13,9 @@ spec = do
         it "returns the same string" $ do
             stringifyField "VALUE" `shouldBe` "VALUE"
 
+        it "preserves the space characters" $ do
+            stringifyField " VALUE\tA  " `shouldBe` " VALUE\tA  "
+
         it "returns an integer as a string" $ do
             stringifyField (1 :: Int) `shouldBe` "1"
 
@@ -24,3 +27,9 @@ spec = do
 
         it "wraps a field value with double quotes if the field contains newline" $ do
             stringifyField "VALUE\nA" `shouldBe` "\"VALUE\nA\""
+
+        it "wraps a field value with double quotes and escape the double quotes if they are used in the field" $ do
+            stringifyField "VALUE\"A" `shouldBe` "\"VALUE\"\"A\""
+
+        it "escapes double quotes even if double quotes are only on the both edges of the field" $ do
+            stringifyField "\"VALUE\"" `shouldBe` "\"\"\"VALUE\"\"\""
